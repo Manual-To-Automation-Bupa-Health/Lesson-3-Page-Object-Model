3645a88c23f5cc364280ff8caf2fdf4408b85e36
Gary Behan <gary.b@browserstack.com>
2022-10-16 23:50:08 +0100

    Initial Commit

---
+++ Drivers/ConfigurationDriver.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace SpecflowBrowserStack.Drivers
+{
+	public class ConfigurationDriver
+	{
+		private const string SeleniumBaseUrlConfigFieldName = "host";
+		private readonly Lazy<IConfiguration> _configurationLazy;
+
+		public ConfigurationDriver()
+		{
+			_configurationLazy = new Lazy<IConfiguration>(GetConfiguration);
+		}
+
+		public IConfiguration Configuration => _configurationLazy.Value;
+
+		public string SeleniumBaseUrl => Configuration[SeleniumBaseUrlConfigFieldName];
+
+		public string Username => Configuration["username"];
+		public string AccessKey => Configuration["access_key"];
+
+		public IEnumerable<IConfigurationSection> CommonCapabilities => Configuration.GetSection("commonCapabilities").GetChildren();
+
+		public IEnumerable<IConfigurationSection> Capabilities => Configuration.GetSection("capabilities").GetChildren();
+
+		private IConfiguration GetConfiguration()
+		{
+			var configurationBuilder = new ConfigurationBuilder();
+
+			configurationBuilder.AddJsonFile(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).FullName, @"browserstack_config.json"));
+
+			return configurationBuilder.Build();
+		}
+	}
+}
